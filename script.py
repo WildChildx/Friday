4adf58c7630c8d4cc0de1b83c23c5aae0c85594d
WildChildx <shreeameybro@gmail.com>
2023-03-19 00:30:24 +0530

    First commit

---
+++ script.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+import wikipedia
+import pyttsx3
+import subprocess
+
+engine = pyttsx3.init()
+recognizer = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Clearing background noise...Please wait")
+    recognizer.adjust_for_ambient_noise(source, duration=1)
+    print("waiting for your message...")
+    recordedaudio = recognizer.listen(source)
+    print('Done recording')
+
+try:
+    print('Printing your message...Please wait')
+    text = recognizer.recognize_google(recordedaudio, language='en-US')
+    print('Your Message:{}', format(text))
+
+except Exception as ex:
+    print(ex)
+
+if 'developer setup' in text.lower():
+    a = "Hello Sir , Initializing developer setup"
+    engine.say(a)
+    engine.runAndWait()
+    program = "C:\\Users\shreec\Documents\AutomaticCommands\startFriday.bat"
+    subprocess.Popen([program])
+
+if 'chrome' in text.lower():
+    a = "Hello Sir , Opening google chrome"
+    engine.say(a)
+    engine.runAndWait()
+    program = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+    subprocess.Popen([program])
+
+
+if 'wikipedia' in text.lower():
+    wikisearch = wikipedia.summary(text)
+    engine.say(wikisearch)
+    engine.runAndWait()
+    program = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+    subprocess.Popen([program])
+# Input data
---
+++ startFriday.bat
@@ -0,0 +1,6 @@
+"C:\Windows\Postman.lnk"
+"C:\Users\shreec\Downloads\sql_management_18_read_v1.rdp"
+start chrome 
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
+"C:\Users\shreec\Desktop\Notion.lnk"
+"C:\\Users\shreec\Desktop\DailyTasks.xlsx"
